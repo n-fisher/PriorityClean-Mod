d81400547bc66c23f98aa0fb5cd46399eb8a80f2
n-fisher <1361631+fyarn@users.noreply.github.com>
2019-11-07 23:51:03 -0800

    Add new settings for specific tiles

--- Source/PriorityClean.cs
+++ Source/PriorityClean.cs
@@ -21,12 +21,18 @@ namespace PriorityClean
 
             Listing_Standard listing_Standard = new Listing_Standard();
             listing_Standard.Begin(inRect);
+
             listing_Standard.CheckboxLabeled("Clean Kitchens: ", ref settings.cleanKitchen);
             listing_Standard.CheckboxLabeled("Clean Hospitals: ", ref settings.cleanHospital);
             listing_Standard.CheckboxLabeled("Clean Laboratories: ", ref settings.cleanLaboratory);
             listing_Standard.CheckboxLabeled("Clean Prison Barracks: ", ref settings.cleanPrisonBarracks);
             listing_Standard.CheckboxLabeled("Clean Cells: ", ref settings.cleanCell);
-            listing_Standard.CheckboxLabeled("Clean all sterile tiles: ", ref settings.cleanAllOtherSterileTiles);
+
+            listing_Standard.CheckboxLabeled("Clean sterile tiles: ", ref settings.cleanSterileTiles);
+            listing_Standard.CheckboxLabeled("Clean silver tiles: ", ref settings.cleanSilverTiles);
+            listing_Standard.CheckboxLabeled("Clean metal tiles: ", ref settings.cleanMetalTiles);
+            listing_Standard.CheckboxLabeled("Clean all other cleanliness tiles: ", ref settings.cleanAllCleanlinessTiles);
+
             listing_Standard.TextFieldNumericLabeled<int>("Max number of tiles to queue in a job (1-50): ", ref settings.jobQueueMax, ref jobQueueMaxString, 1, 50);
             listing_Standard.TextFieldNumericLabeled<int>("Radius of tiles to scan for filth (1-10): ", ref settings.jobScanRadius, ref jobScanRadiusString, 1, 10);
             listing_Standard.End();
--- Source/PriorityCleanSettings.cs
+++ Source/PriorityCleanSettings.cs
@@ -9,7 +9,10 @@ namespace PriorityClean
         public bool cleanPrisonBarracks = false;
         public bool cleanCell = false;
         public bool cleanKitchen = true;
-        public bool cleanAllOtherSterileTiles = true;
+        public bool cleanSterileTiles = true;
+        public bool cleanMetalTiles = false;
+        public bool cleanAllCleanlinessTiles = false;
+        public bool cleanSilverTiles = false;
         public int jobQueueMax = 15;
         public int jobScanRadius = 15;
 
@@ -18,10 +21,13 @@ namespace PriorityClean
             base.ExposeData();
             Scribe_Values.Look(ref cleanKitchen, "cleanKitchen", true);
             Scribe_Values.Look(ref cleanLaboratory, "cleanLaboratory", true);
-            Scribe_Values.Look(ref cleanPrisonBarracks, "cleanPrisonBarracks", true);
+            Scribe_Values.Look(ref cleanPrisonBarracks, "cleanPrisonBarracks", false);
             Scribe_Values.Look(ref cleanHospital, "cleanHospital", true);
-            Scribe_Values.Look(ref cleanCell, "cleanCell", true);
-            Scribe_Values.Look(ref cleanAllOtherSterileTiles, "cleanAllOtherSterileTiles", true);
+            Scribe_Values.Look(ref cleanCell, "cleanCell", false);
+            Scribe_Values.Look(ref cleanSterileTiles, "cleanSterileTiles", true);
+            Scribe_Values.Look(ref cleanMetalTiles, "cleanMetalTiles", false);
+            Scribe_Values.Look(ref cleanAllCleanlinessTiles, "cleanAllCleanlinessTiles", false);
+            Scribe_Values.Look(ref cleanSilverTiles, "cleanSilverTiles", false);
             Scribe_Values.Look(ref jobQueueMax, "jobQueueMax", 15);
             Scribe_Values.Look(ref jobScanRadius, "jobScanRadius", 5);
         }
